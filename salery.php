d01d340b2eaa92107d53c3bd2475eb95894b2108
P. A Diluna Sithija Sulakshana <dilunasithija111@gmail.com>
2025-04-19 10:50:40 +0530

    text correction update

--- salery.php
+++ salery.php
@@ -166,7 +166,7 @@
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-5 " id="main-dev">
                     <div class="row ">
                         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                            <h1 class="h2">Salery</h1>
+                            <h1 class="h2">Salary</h1>
                             <div class="btn-toolbar mb-2 mb-md-0">
                                 <!-- <div class="btn-group me-2">
                   <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -192,14 +192,25 @@
     WHERE users_u_id = '$userId' AND status_s_id = 1
 ");
                             $monthCountRow = $monthCountResult->fetch_assoc();
+                            
                             $monthCount = $monthCountRow['month_count'] ?? 0;
+                            if($monthCount >1 ){
+                                $monthCount = $monthCount -1;
+
+                            }else{
+                                $monthCount = $monthCount+1;
+                            }
+                    
+
+
+                         
                             ?>
 
                             <div class="accordion" id="commissionAccordion">
                                 <?php
                                 $cumulativeCommission = 0;
 
-                                for ($i = $monthCount - 1; $i >= 0; $i--):
+                                for ($i = $monthCount; $i >= 0; $i--):
                                     $monthStart = date('Y-m-01', strtotime("-$i months"));
                                     $monthEnd = date('Y-m-t', strtotime("-$i months"));
                                     $monthName = date('F Y', strtotime($monthStart));
