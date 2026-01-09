3b791e6afd1b56b17287c9f83d08bc0743826253
P. A Diluna Sithija Sulakshana <dilunasithija111@gmail.com>
2026-01-09 19:14:20 +0530

    update db name

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ class Database{
 
     public static function setupConnection() {
         if(!isset(Database::$connection)){
-            Database::$connection=new mysqli("localhost","root","Corei7diluna09","new_systerm_sa","3306");
+            Database::$connection=new mysqli("localhost","root","Corei7diluna09","new_systerm_sa_new","3306");
 
         }
     }
